61bc45786193c8ff14f1b9d4e0b2ae66655d52f3
rZoysa <rithikzoysa@gmail.com>
2025-03-27 23:38:36 +0530

    Enhance Rocket model with additional attributes

--- lib/models/rockets.dart
+++ lib/models/rockets.dart
@@ -2,14 +2,28 @@ class Rocket {
   final String id;
   final String name;
   final bool activeStatus;
+  final int costPerLaunch;
+  final int successRate;
+  final String firstFlight;
+  final double heightMeters;
+  final double diameterMeters;
+  final int massKg;
   final String description;
+  final String wikipedia;
   final List<String> images;
 
   Rocket({
     required this.id,
     required this.name,
     required this.activeStatus,
+    required this.costPerLaunch,
+    required this.successRate,
+    required this.firstFlight,
+    required this.heightMeters,
+    required this.diameterMeters,
+    required this.massKg,
     required this.description,
+    required this.wikipedia,
     required this.images,
   });
 
@@ -18,7 +32,14 @@ class Rocket {
       id: json['id'].toString(),
       name: json['rocket_name'],
       activeStatus: json['active'] as bool,
+      costPerLaunch: json['cost_per_launch'] as int,
+      successRate: json['success_rate_pct'] as int,
+      firstFlight: json['first_flight'],
+      heightMeters: (json['height']['meters'] as num).toDouble(),
+      diameterMeters: (json['diameter']['meters'] as num).toDouble(),
+      massKg: json['mass']['kg'] as int,
       description: json['description'],
+      wikipedia: json['wikipedia'],
       images: List<String>.from(json['flickr_images']),
     );
   }
